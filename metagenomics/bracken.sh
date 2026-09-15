dd0100771b64bb1b47798d59b2f69e612650050a
filippob <filippo.biscarini@gmail.com>
2026-09-15 09:22:27 +0200

    commented script

--- metagenomics/bracken.sh
+++ metagenomics/bracken.sh
@@ -1,16 +1,19 @@
+## This is needed if nf-core/taxprofiler was run without bracken
+## Only the different databases (kraken2, metaphlan, mOTUs etc.)
+## bracken will process kraken2 output to prduce count tables (one per sample) 
 
 echo "$HOME"
+DATABASE=taxprofiler/databases/kraken2
 
 for R in taxprofiler/kraken2/db2/*;
 do
 	SAMPLE=$(basename $R .report)
 	echo $SAMPLE 
 	bracken \
-	    -d /path/to/kraken2_db \
+	    -d $DATABASE \
 	    -i $R \
 	    -o bracken_outputs/${SAMPLE}.bracken \
 	    -w bracken_outputs/${SAMPLE}.bracken.report \
-	#   -r 150 \
 	    -l S
 done
 
